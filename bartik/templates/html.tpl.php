b542529c03fe927ea123ac7b34190057b51095bf
Ian Thompson <ian@ariasthompson.com>
2018-08-01 16:03:12 +0100

    Cleaned html.tpl.php

--- bartik/templates/html.tpl.php
+++ bartik/templates/html.tpl.php
@@ -1,37 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link rel="icon" type="image/png" href="/themes/ariasthompson/img/170518_ariasthompson_newFavIcon.png">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
-  <meta name="author" content="Relational Path Institute">
-  <meta property="og:title" content="Relational Path Institute" />
-  <link rel="icon" type="image/x-icon" href="/themes/relationalpath/relational-path-favicon.ico" />
-  <link rel="icon" type="image/png" href="/themes/relationalpath/relational-path-favicon.png" />
+  <meta name="author" content="Maria Paula Arias">
+  <meta property="og:title" content="Maria Paula Arias" />
+
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Noto+Sans:400,700" rel="stylesheet">
+
   <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
   <?php print $styles; ?>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <?php print $scripts; ?>
-  <style type="text/css">
-	#mc_embed_signup{clear:left;}
-  </style>
-  <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/2e89861317ca3175fc15940d2/750256a7528dfcc5c0cbb648e.js");</script>
-  <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-110489840-1', 'auto');
-      ga('send', 'pageview');
-  </script>
-  
+  <?php print $scripts; ?>  
 </head>
 
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
